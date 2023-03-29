baef1eec42c3ca9eefda5d90593b1eecf32dd170
emreb <emre.buyuktas@devbase.com.tr>
2023-03-29 22:44:45 +0300

    Start implementation of command pattern

---
+++ CommandPattern/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CommandPattern.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
